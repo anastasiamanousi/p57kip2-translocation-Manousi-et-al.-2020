4d0fd0b191cfd7284c5cf507491bad35681d9d3b
anastasiamanousi <74628290+anastasiamanousi@users.noreply.github.com>
2021-04-05 17:53:11 +0200

    Update p57kip2 translocation_Manousi et al., 2020.m

--- p57kip2 translocation_Manousi et al., 2020.m
+++ p57kip2 translocation_Manousi et al., 2020.m
@@ -2,7 +2,7 @@
 %p57kip2 localization in one well, give that the p57kip2 signal intensity
 %in the two regions (nucl vs. cyto) is provided as input.
 %The only parameter that changes is the threshold nucl(line 14), which
-%compencates for differences in the overall signal intensity among the
+%compensates for differences in the overall signal intensity among the
 %plates. This threshold is being set upon manual analysis of the positive and
 %negative controls and under our experimental conditions was always 1000+/-
 %200. 
